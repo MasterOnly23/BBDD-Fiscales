60fea802596c92aa377329e39993c05002f43d5d
Pipe Daza <pipedaza23@gmail.com>
2022-09-14 23:50:54 -0300

    cambio nombre proyecto

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MVT_JuanDaza.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BBDD_Fiscales.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
