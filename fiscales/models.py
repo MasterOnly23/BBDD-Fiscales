074e500cb65f52533bf9c50cc4a69e01cce69061
Pipe Daza <pipedaza23@gmail.com>
2022-09-15 00:22:08 -0300

    modificaciones template y models name

--- fiscales/models.py
+++ fiscales/models.py
@@ -7,10 +7,10 @@ from django.db import models
 # Create your models here.
 
 class Fiscales(models.Model):
-    nombre_suc = models.CharField(max_length=50) #le pasamos el tipo de dato de cada variable
-    nro_suc = models.IntegerField()
+    nombre_sucursal = models.CharField(max_length=50) #le pasamos el tipo de dato de cada variable
+    numero_sucursal = models.IntegerField()
     modelo_fiscal = models.CharField(max_length=40)
     punto_venta = models.IntegerField()
-    nro_serie = models.IntegerField()
+    numero_serie = models.IntegerField()
     fecha_cambio = models.DateField()
     estado = models.CharField(max_length=40)
