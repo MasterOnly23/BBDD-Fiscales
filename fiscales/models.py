59f9836ce003dde8ecc749cc7c33eb681add251a
jfdaza <mainekko23@gmail.com>
2022-09-19 13:01:47 -0300

    barra busqueda - terminado

--- fiscales/models.py
+++ fiscales/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class Fiscales(models.Model):
     nombre_sucursal = models.CharField(max_length=50) #le pasamos el tipo de dato de cada variable
-    numero_sucursal = models.IntegerField()
+    numero_sucursal = models.CharField(max_length=5)
     modelo_fiscal = models.CharField(max_length=40)
     vencimiento_certificado_digital = models.DateField(blank=True,null=True)
     punto_venta = models.IntegerField()
